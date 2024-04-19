f8d2221581f7c6315968ebf8d4509fa552eb4f95
LucasMonnais <lucas.monnais@efrei.net>
2024-04-20 01:03:15 +0200

    implémentation de toutes les fonctions pour colonne + correction de bug sur le realloc dans insertion et correction de suppression de colonne

--- column.c
+++ column.c
@@ -1,14 +1,11 @@
 //
 // Created by anton on 28/03/2024.
 //
+#include <stdio.h>
 #include <stdlib.h>
 #include "column.h"
 
-/**
-* Create a column
-* @param1 : Column title
-* @return : Pointer to created column
-*/
+
 COLUMN *create_column(char* title){
 
     COLUMN* create = malloc(sizeof(COLUMN));
@@ -18,4 +15,90 @@ COLUMN *create_column(char* title){
     create->donnees = NULL;
 
     return create;
+}
+
+
+int insert_value(COLUMN* col, int value){
+    if (col->donnees == NULL) {
+        col->donnees = malloc(col->TP * sizeof(int));
+        if(col->donnees == NULL){
+            return 0; //imposibilité d'allouer de la mémoire = manque de mémoire
+        }
+        else {
+            col->donnees[col->TL] = value;
+            col->TL++;
+            return 1;
+        }
+    }
+    if (col->TP == col->TL){
+        col->TP += 256;
+        int* temp = realloc(col->donnees, col->TP * sizeof(int));
+        if (temp == NULL){
+            return 0;
+        }
+        else {
+            col->donnees = temp;
+            col->donnees[col->TL] = value;
+            col->TL++;
+            return 1;
+        }
+
+
+    }
+    col->donnees[col->TL] = value;
+    col->TL++;
+    return 1;
+
+}
+
+
+void delete_column(COLUMN **col){
+    free((*col)->donnees);
+    (*col)->donnees = NULL;
+    free(*col);
+    *col = NULL;  //Cette ligne est fausse erreur à creuser -> erreur corrigé le 20/04 : ajout de *
+}
+
+
+
+void print_col(COLUMN* col){
+    for (int i = 0; i<col->TL ;i++) {
+        printf("[%d]  %d \n", i,  col->donnees[i]);
+    }
+}
+
+int nombre_occurence(COLUMN* col, int x){
+    int occ = 0;
+    for (int i = 0; i<col->TL ;i++) {
+        if (x == col->donnees[i] ){
+            occ++;
+        }
+    }
+    return occ;
+}
+
+int recherche_position(COLUMN* col, int x){
+
+    printf("[%d]  %d \n", x,  col->donnees[x]);   //pas col->donnees[x+1] ?
+
+}
+
+int nombre_valeurs_supperieures(COLUMN* col, int x){
+    int occ = 0;
+    for (int i = 0; i<col->TL ;i++) {
+        if (x < col->donnees[i] ){
+            occ++;
+        }
+    }
+    return occ;
+}
+
+int nombre_valeurs_inferieures(COLUMN* col, int x){
+    int occ = 0;
+    for (int i = 0; i<col->TL ;i++) {
+        if (x > col->donnees[i] ){
+            occ++;
+        }
+    }
+    return occ;
 }
--- column.h
+++ column.h
@@ -19,6 +19,34 @@ typedef struct {
 */
 COLUMN *create_column(char* title);
 
+/**
+* @brief : Add a new value to a column
+* @param1 : Pointer to a column
+* @param2 : The value to be added
+* @return : 1 if the value is added 0 otherwise
+*/
+int insert_value(COLUMN* col, int value);
+
+/**
+* @brief : Free allocated memory
+* @param1 : Pointer to a column
+*/
+void delete_column(COLUMN **col);
+
+/**
+* @brief: Print a column content
+* @param: Pointer to a column
+*/
+void print_col(COLUMN* col);
+
+int nombre_occurence(COLUMN* col, int );
+
+int recherche_position(COLUMN* col, int );
+
+int nombre_valeurs_supperieures(COLUMN* col, int x);
+
+int nombre_valeurs_inferieures(COLUMN* col, int x);
+
 #endif //UNTITLED_COLUMN_H
 
 
