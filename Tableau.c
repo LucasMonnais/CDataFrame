281fa7f97053fde1479de4b5943609fc585e9ff0
LucasMonnais <lucas.monnais@efrei.net>
2024-04-19 17:37:27 +0200

    Initial commit

---
+++ Tableau.c
@@ -0,0 +1,19 @@
+//
+// Created by anton on 28/03/2024.
+//
+
+#include "Tableau.h"
+
+/**
+* Create a column
+* @param1 : Column title
+* @return : Pointer to created column
+*/
+COLUMN *create_column(char* title){
+    COLUMN create;
+    create.titre = title;
+    create.TP = 256;
+
+
+
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "Tableau.h"
+/* bonne chance a nous (36 pages :) */
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+
